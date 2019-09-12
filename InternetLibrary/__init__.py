59f6a86ec978887c49bffb763e9db8e571fe7b7e
Anderson Alves <andersonalves4608@gmail.com>
2019-09-12 15:27:59 -0300

    Init file

---
+++ InternetLibrary/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask, Blueprint
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+
+
+app = Flask(__name__)
+CORS(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://ADMIN:PASS@0.0.0.0:5432/Borges'
+db = SQLAlchemy(app)
+
+from InternetLibrary.Authors.Views.AuthorViews import author
+from InternetLibrary.Book.Views.BookViews import book
+app.register_blueprint(author)
+app.register_blueprint(book)
+
+db.create_all()
