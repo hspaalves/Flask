aefcbcf55d6365d86c36f784320c0ddbbd1e2bc9
Anderson Alves <andersonalves4608@gmail.com>
2019-09-16 17:44:41 -0300

    retirando função não utilizada

--- InternetLibrary/Authors/Serializer/AuthorSerializer.py
+++ InternetLibrary/Authors/Serializer/AuthorSerializer.py
@@ -7,18 +7,6 @@ class AuthorSerializer:
         pass
 
     @staticmethod
-    def get_not_id(authors):
-        res = {'count': len(authors), 'next': None, 'previous': None}
-        results = []
-        for detail_author in authors:
-            results.append({
-                'id': detail_author.id,
-                'name': detail_author.name
-            })
-            res['results'] = results
-        return res
-
-    @staticmethod
     def get_by_id(authors):
         if not authors:
             abort(404)
